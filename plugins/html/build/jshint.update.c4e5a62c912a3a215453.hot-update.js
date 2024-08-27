3e2853d646d04ceb42548ed952c4b233beeca953
Danny Guzman <guzmanjd86@gmail.com>
2024-08-27 04:22:39 -0700

    Updated plugins

---
+++ plugins/html/build/jshint.update.c4e5a62c912a3a215453.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate_caweb_html_webpack_plugin"]("jshint.update",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("60ca0543809eca3e47c2")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=jshint.update.c4e5a62c912a3a215453.hot-update.js.map
