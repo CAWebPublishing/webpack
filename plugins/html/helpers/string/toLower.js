8457072e19ced82a4e7168a711a9c41b6ae1dc43
Danny Guzman <guzmanjd86@gmail.com>
2024-11-05 12:21:29 -0800

    Refined toLower, toUpper, toTitleCase, replace helper functions

--- plugins/html/helpers/string/toLower.js
+++ plugins/html/helpers/string/toLower.js
@@ -1,6 +1,16 @@
-export default function toLower(value){
-    // if value is passed 
+export default function toLower(value, options){
+    let response = '';
+
+    // if parameter is passed 
     if( value && value.length ){
-        return value.toLowerCase();
+        response = value.toLowerCase();
     }
+
+    // if nested content exists
+    if( (value.fn && value.fn().length) || (options.fn && options.fn().length ) ){
+        let content = value.fn ? value.fn() : options.fn();
+        response +=  content.toLowerCase();
+    }
+
+    return response;
 }
